a22fda5e411d8be648c73022f17f3410b60aec8b
pankil-singh-atwork <pankilkumar.singh2020@vitstudent.ac.in>
2023-10-07 15:24:49 +0530

    bolg js deleted

--- js/contact.js
+++
@@ -1,7 +0,0 @@
-const sendMessage = (e) => {
-    e.preventDefault();
-    console.log(document.getElementById('name').value);
-    console.log(document.getElementById('email-id').value);
-    console.log(document.getElementById('number').value);
-    console.log(document.getElementById('message').value);
-}
